aa214350d71227efce1a3e78ab94fbc6bd87c522
Matthew <malambert@csumb.edu>
2019-09-23 17:42:43 -0700

    flixter part2

---
+++ flix/flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Matthew Lambert on 9/23/19.
+//  Copyright © 2019 Matthew Lambert. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
